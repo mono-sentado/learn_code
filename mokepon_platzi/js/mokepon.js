572da29123739f235fe5522ee2b1b715764e68c7
mono-sentado <luismileonruiz@gmail.com>
2024-06-26 13:35:05 +0200

    actualización 13:34 26/06/2024

--- mokepon_platzi/js/mokepon.js
+++ mokepon_platzi/js/mokepon.js
@@ -1,6 +1,33 @@
-function seleccionarMonstruoJugador() {
-    alert('Seleccionaste tu Mostruo')
+
+
+function iniciarJuego() {
+    let botonMonstruoJugador = document.getElementById('bmonstruo')
+    botonMonstruoJugador.addEventListener('click', seleccionarMonstruoJugador)
 }
 
-let botonMonstruoJugador = document.getElementById('bmonstruo')
-botonMonstruoJugador.addEventListener('click', seleccionarMonstruoJugador)
+function seleccionarMonstruoJugador() {
+    let inputFosfyah = document.getElementById('Fosfyah').checked
+    let inputRioblis = document.getElementById('Rioblis').checked
+    let inputHojurot = document.getElementById('Hojurot').checked
+    let inputLavasee = document.getElementById('Lavasee').checked
+    let inputRocicry = document.getElementById('Rocicry').checked
+    let inputClipperwood = document.getElementById('Clipperwood').checked
+
+    if(inputFosfyah){
+        alert("Seleccionaste Fosfyah")
+    } else if(inputRioblis){
+        alert("Seleccionaste Rioblis")
+    } else if(inputHojurot){
+        alert("Seleccionaste Hojurot")
+    } else if(inputLavasee){
+        alert("Seleccionaste Lavasee")
+    } else if(inputRocicry){
+        alert("Seleccionaste Rocicry")
+    } else if(inputClipperwood){
+        alert("Seleccionaste Clipperwood")
+    } else {
+        alert("Selecciona algun Mostruo")
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
