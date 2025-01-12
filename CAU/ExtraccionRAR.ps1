f3587b04fb93ef58385e3755a18fb2f04b35456c
mono-sentado <luismileonruiz@gmail.com>
2025-01-12 10:47:51 +0100

    Actualizacion 12/01/2025 10:47

--- CAU/ExtraccionRAR.ps1
+++
@@ -1,2 +0,0 @@
-echo "Descomprimiendo archivo mls.rar"
-& "C:\Program Files\7-Zip\7z.exe" x "C:\Instalaciones_ps1\4_Personal Communication\mls.rar"
--- CAU/IniAppCAU/INI_CAU/INI_CAU/INI_CAU_AUTO.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File %~dp0INI_CAU_AUTO.ps1
-timeout t 5 nobreak nul
-exit
