ff6dbc4c1838c4b240b3ad6ceb6760383693aaa3
mono-sentado <luismileonruiz@gmail.com>
2025-01-12 11:42:34 +0100

    Actualizar INI_CAU_MANUAL.ps1

--- INI_CAU/INI_CAU_MANUAL.ps1
+++ INI_CAU/INI_CAU_MANUAL.ps1
@@ -28,35 +28,18 @@ while ($true) {
 			$answer = Read-Host "Ejecutar Procesos? (s/n)"
 			if ($answer -eq "s") {
 				# Ejecuta Caffeine
-			Start-Process "C:\Users\$env:USERNAME\Desktop\caffeine64 4.exe"
-				# Ejecuta LockStatus
-			Start-Process "C:\Users\$env:USERNAME\Desktop\lockoutstatus.exe"
-				# Ejecuta LAPS UI
-			Start-Process "C:\Program Files\LAPS\AdmPwd.UI.exe"
-				# Ejecuta Lotus Notes y Domino Admin
-			Start-Process "C:\Program Files (x86)\IBM\Notes\admin.exe"
-			Start-Process "C:\Program Files (x86)\IBM\Notes\notes.exe"
-				# Ejecuta Apache
-			Start-Process "\\$env:COMPUTERNAME\c$\Program Files (x86)\Apache Directory Studio\Apache Directory Studio.exe"
-				# Ejecuta Ivanti
-			Start-Process "C:\Program Files\LANDesk\ManagementSuite\Console.exe"
-				# Ejecuta Avaya
-			Start-Process "C:\Program Files (x86)\Avaya\Avaya one-X Agent\OneXAgentUI.exe"
+			Start-Process "C:\Users\$env:USERNAME\Desktop\caffeine64 4.exe" 
 				# Ejecuta Directorio Activo
 			Start-Process dsa.msc
 				# Ejecuta 2 Asistencia Rápida
 			Start-Process "ms-quick-assist:"
-			Start-Process "ms-quick-assist:"
 				# Ejecuta Teams
 			Start-Process "ms-teams"
-				# Ejecuta GRTD TC4
-			Start-Process "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe" "10.4.100.45"
+				# Ejecuta GRTD
+			Start-Process "C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe" "0.0.0.0"
     
-				# Abre Navegadores
-			Start-Process msedge "https://acceso.averroes.lda/Login/index.html"
+				# Abre Navegador
 			Start-Process msedge "https://outlook.office.com/mail/sentitems"
-			Start-Process msedge "https://wiki.lda/pages/viewpage.action?spaceKey=CAU&title=MULTICLIENTE"
-			Start-Process chrome "https://controldesk.lda/maximo/ui/?event=loadapp&value=startcntr&uisessionid=2513&_tt=vik7sbjgdp3d4d9rbe99ll90ou"
 
 				# Abre archivos de .txt cambiar la ruta
 			Start-Process notepad.exe "C:\Users\$env:USERNAME\Ruta\Archivo.txt"
